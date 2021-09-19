0ed6aacf80effcf521cf6f101b3ad1def4716977
qiuxan <qiuxan@gmail.com>
2021-09-19 16:10:55 +1000

    class name setup features

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 
 class Counter extends React.Component {
+    state={
+        count:0,
+    };
+
+    // style={
+    //     fontSize:50,
+    //     fontWeight:'bold',
+    // }
+
+
+
     render() { 
-        return <h1>Hello World</h1>;
+        return (
+            <React.Fragment>
+            <span  className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increament</button>
+        </React.Fragment>
+        );
     }
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+     formatCount(){
+            // console.log('formatCount here');
+            const {count}=this.state;
+            // console.log(this.state.count);
+            return count===0?'Zero':count;
+
+        }
 }
  
 export default Counter;
